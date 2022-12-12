349e57972146a921be3a4ab08abf0ff3174ccd2b
Antonino Fulci <antonino.fulci@unime.it>
2022-12-12 09:44:13 +0100

    Inizio

---
+++ generate_run.py
@@ -0,0 +1,79 @@
+#!/usr/local/bin/python3
+
+import argparse, os, random
+
+# Questa funzione genera il file di input per una nuova run
+# praticamente gli cambia la line "RANDOMIZ" mettendogli un seed diverso random
+# possibile miglioramento da fare per garantire che tutti i job abbiano sempre un seed diverso:
+# creare un file con tutti i seed da usare e leggerli da lì
+# nel file: generare un seed e scriverlo solo se è diverso da tutti i precedenti    
+
+def GenerateInput(input, iteration):
+
+    #   minimo e massimo valore di seed accettati da fluka
+    MIN_RAND=1.
+    MAX_RAND=9E7
+
+    #   generazione del seed della run
+    seed=random.randint(MIN_RAND,MAX_RAND)
+
+    #   generazione della stringa col nuovo seme
+    new_randomiz = "RANDOMIZ          1.{:>10n}\n".format(seed)             #":"->inizio formato; ">"->allineato a destra; "10"->dieci spazi da occupare; "n"->numero da mettere
+
+    with open(input+".inp", "r+") as f:                                     #apre il file in modalità lettura e scrittura
+        data = f.readlines()                                                #legge tutte le linee e le mette in una lista
+        for index, line in enumerate(data):                                 #cicla su tutte le linee, il doppio ciclo permette di avere su index l'indice del ciclo e su line l'elemento della lista
+            if "RANDOMIZ" in line:                                          #se trova "RANDOMIZ" allora si attiva
+                data[index] = new_randomiz                                  #quindi sostituisce la linea col nuovo randomiz nella lista
+                break                                                       #interrompe il ciclo
+
+        f.seek(0)                                                           #si mette a scrivere dall'inizio
+        f.writelines(data)                                                  #scrive tutte le linee in data
+        f.truncate()                                                        #serve ma non so che faccia
+
+    file_name = input+"_{:04d}".format(iteration)+".inp"                    #genera il nome del nuovo file di input
+    os.system("mv " + input + ".inp "+ file_name)                           #rinomina il file
+
+    return file_name                                                        #ritorna il nome del file per usarlo nell'altro metodo
+
+
+def GenerateSh(input, iteration, fluka_path, custom_exe = "None"):
+    # genera il file sh che lancia il comando trovato sul sito: https://fluka.cern/documentation/running/fluka-command-line
+    # /pathtofluka/bin/rfluka -M 5 -e ./myfluka example.inp
+
+    # generazione del job da lanciare
+    nome = "job_" + str(iteration) + ".sh"                                  # crea il nome del file in base all'iterazione corrente
+    sh = open(nome, "w")                                                    # crea il file
+
+    # scrittura nel file dei comando da usare
+    sh.write("#!/usr/bin/env bash")                                         
+    sh.write("\n\n")
+    sh.write(fluka_path + " -M 1 -e ." + custom_exe + " " + input)          # creazione comando
+
+    sh.close()                                                              # chiusura del file
+
+    # rende il file eseguibile
+    os.system("chmod +x " + nome)
+
+# Serve per poter lanciare lo script con degli arguments
+parser = argparse.ArgumentParser(description='Fluka cern run launch script')
+
+# Definizione degli argument da accettare
+parser.add_argument('--input',      type=str, required=True, help="Input file name for fluka")
+parser.add_argument('--iteration',  type=int, required=True, help='Current iteration')
+parser.add_argument('--fluka',      type=str, required=True, help='Path to the fluka-cern path')
+parser.add_argument('--custom_exe', type=str, required=True, help='Path to the custom exe')
+args = parser.parse_args()
+
+INPUT_FILE  = args.input            #file name, stripped of the .inp
+C_ITERATION = args.iteration        #current iteration
+FLUKA_PATH  = args.fluka            #fluka-cern path
+CUSTOM_EXE  = args.custom_exe       #fluka custom exe
+
+# Lanciamo prima la funzione per generare il nuovo file di input con un seed random
+NEW_INPUT = GenerateInput(INPUT_FILE, C_ITERATION)
+
+# Creiamo il file sh che verrà lanciato sulla farm come job
+GenerateSh(NEW_INPUT, C_ITERATION, FLUKA_PATH, CUSTOM_EXE)
+
+
---
+++ launch.sh
@@ -0,0 +1,178 @@
+#!/usr/bin/env bash
+
+################################################################################
+# Program options                                                              #
+################################################################################
+#path to the fluka-cern executable and the custom exe
+fluka_path="/path/to/fluka"                         #con "/" all'inizio
+custom_exe="/path/to/exe"                           #con "/" all'inizio
+
+#path to the python script that generates the runs
+pyscript="/Users/antoninofulci/FlukaWork/NewRunPipes/generate_run.py"
+
+#wirte here the queue where to launch the jobs
+LSF_QUEUE="long"
+
+#some options
+Overwriting="n"         #Overwrite the file for each run: y/n
+ERR_FILE="err.txt"
+OUT_FILE="out.txt"
+
+# Versione script
+version=0.9
+
+# Valori di default di sicurezza
+file=0
+job_number=0
+directory=0
+verbose=false
+Version=false
+Defa=false
+
+################################################################################
+# Help                                                                         #
+################################################################################
+Help(){
+   # Display Help
+   echo "This script launch a simulation on the farm."
+   echo
+   echo "Syntax:"
+   echo "./launch.sh -f <fluka_input_file>.input -j <number of job> [-d <simulation/directtory/path/>|-v|-V|-D]"
+   echo
+   echo "Required arguments:"
+   echo "f     Fluka input file (must ends in .inp)"
+   echo "j     Number of jobs to run (must be >0)"
+   echo ""
+   echo "Optional arguments"
+   echo "d     Directory where to save the simulations run/s"
+   echo "v     Verbose mode."
+   echo "V     Print software version and exit."
+   echo "D     Print default variables values"
+   echo
+}
+
+Defaults(){
+    echo "Default file: $file"
+    echo "Default job_number $job_number"
+    echo "Default directory ./file_name/"
+    echo "Default verbose = $verbose"
+    echo "Default FLUKA-CERN executable path: None. Must be inserted"
+    echo "Default FLUKA custom executable path: None. Must be inserted"
+    exit 0
+}
+
+################################################################################
+# Launching jobs function                                                      #
+################################################################################
+
+# Funzione per ottenere il path assoluto del file, trovata su internet, funziona.
+get_abs_filename() {
+  # $1 : relative filename
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+# Funzione che lancia le varie simulazioni sulla farm
+Launch(){
+    # Strippa il nome del file di input
+    StrippedName=${file%.*}
+
+    if [ $directory == 0 ]
+    then
+        directory=$StrippedName
+    fi
+    
+    # Piccolo controllo prima di far partire lo script
+    echo "The file chosen is: $file"
+    echo "The number of job to launch is $job_number"
+    echo "The simulation will be saved in the new directory: $(pwd)/$directory" 
+    echo "Is it correct? [y/n]"
+    read response
+
+    if [ $response == "n" ]
+    then
+        echo "Exiting bye!"
+        exit
+    fi
+
+    # Se si vuole sovrascrivere i file err.txt e out.txt
+    if [ $Overwriting == "y" ]
+    then
+        err_opt="-eo"
+        out_opt="-oo"
+    else
+        err_opt="-e"
+        out_opt="-o"
+    fi
+
+
+    # Ottiene il path assoluto del file di input
+    FileAbsPath=$(get_abs_filename "$file")
+
+    # Creala cartella che conterrà la simulazione e ci entra, se l'opzione -d è passata con un valore diverso da 0 allora la creerà con quel nome
+    mkdir ./$directory
+    cd $directory
+
+    # Cicla sul numero di job che si vuole lanciare
+    for (( i = 0001; i <= $job_number; i++ )); do
+        
+        # Crea il nome della cartella del job come 0001 in modo che poi appaiano in ordine
+        job=`printf ./job_%04d $i`
+        mkdir $job
+        cd $job
+        
+        # Copia il file dentro la cartella
+        cp $FileAbsPath .
+
+        # Script python che genera il file di input con un nuovo seed e il file .sh per avviare la simulazione
+        python3 $pyscript --input=$StrippedName --iteration=$i --fluka=$fluka_path --custom_exe=$custom_exe 
+
+        # Lancia la simulazione
+        echo bsub -P c7 -q $LSF_QUEUE -M 8192 -R "select[mem>8192] rusage[mem=8192]" $err_opt $ERR_FILE $out_opt $OUT_FILE ./job_$i.sh
+
+        # Torna indietro in modo da poter rieseguire tutto da capo
+        cd ".."
+
+    done
+}
+
+################################################################################
+################################################################################
+# Main program                                                                 #
+################################################################################
+################################################################################
+
+# Ottiene le opzioni dal terminale. Quelle con i ":" vogliono qualcosa, quelle con "," sono ad attivazione
+while getopts f:j:d:v,V,D flag
+do
+    case "${flag}" in
+        f) file=${OPTARG};;
+        j) job_number=${OPTARG};;
+        d) directory=${OPTARG};;
+        v) verbose=true;;
+        V) Version=true;;
+        D) Defa=true;;
+    esac
+done
+
+# Se l'opzione -D è passata al programma stampa a schermo i valori di default
+if [ $Defa == true ]
+then
+    Defaults
+fi
+
+# Se l'opzione -V è passata al programma stampa a schermo la versione dello script
+if [ $Version == true ]
+then
+    echo "Launching scirpt version: $version"
+    exit 0
+fi
+
+#Checking the correct usage of the script
+if [ "${file: -4}" != ".inp" -o $job_number -le 0 ]
+then
+    echo "Error!!!"
+    echo 
+    Help
+else
+    Launch
+fi
